69e68e50b314fc8cfa791cca96f1cc5fe96b7fd4
Congglee <congldqn888@gmail.com>
2025-02-18 10:57:31 +0700

    Initialize Express application with environment configuration and logging

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,34 @@
-const name: string = 'Conggglee'
-console.log(name)
+import { Server } from 'http'
+import app from '~/app'
+import { envConfig } from '~/config/environment'
+import logger from '~/config/logger'
+
+const server: Server = app.listen(envConfig.port, () => {
+  logger.success(`Listening to port ${envConfig.port}`)
+})
+
+const exitHandler = () => {
+  if (server) {
+    server.close(() => {
+      logger.info('Server closed')
+      process.exit(1)
+    })
+  } else {
+    process.exit(1)
+  }
+}
+
+const unexpectedErrorHandler = (error: unknown) => {
+  logger.error(error instanceof Error ? error.stack || error.message : String(error))
+  exitHandler()
+}
+
+process.on('uncaughtException', unexpectedErrorHandler)
+process.on('unhandledRejection', unexpectedErrorHandler)
+
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM received')
+  if (server) {
+    server.close()
+  }
+})
