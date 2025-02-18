b2bdf9cfcc34434f061851ef2fbe546eea0ffad6
Congglee <congldqn888@gmail.com>
2025-02-18 23:08:40 +0700

    Implement Prisma client integration and database connection handling

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,17 @@
 import express from 'express'
+import prisma from '~/client'
 
 const app = express()
 
 app.use(express.json())
+
+async function main() {
+  const allUsers = await prisma.user.findMany()
+  console.log(allUsers)
+}
+
+main().catch((error) => {
+  console.error('Error in main function:', error)
+})
 
 export default app
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,30 @@
 import { Server } from 'http'
 import app from '~/app'
+import prisma from '~/client'
 import { envConfig } from '~/config/environment'
 import logger from '~/config/logger'
 
-const server: Server = app.listen(envConfig.port, () => {
-  logger.success(`Listening to port ${envConfig.port}`)
+let server: Server
+
+prisma.$connect().then(async () => {
+  logger.success('Connected to SQL Database')
+
+  server = app.listen(envConfig.port, () => {
+    logger.success(`Listening to port ${envConfig.port}`)
+  })
 })
 
-const exitHandler = () => {
+const exitHandler = async () => {
   if (server) {
-    server.close(() => {
+    server.close(async () => {
       logger.info('Server closed')
+      await prisma.$disconnect()
+      logger.info('Database connection closed')
       process.exit(1)
     })
   } else {
+    await prisma.$disconnect()
+    logger.info('Database connection closed')
     process.exit(1)
   }
 }
@@ -26,9 +37,11 @@ const unexpectedErrorHandler = (error: unknown) => {
 process.on('uncaughtException', unexpectedErrorHandler)
 process.on('unhandledRejection', unexpectedErrorHandler)
 
-process.on('SIGTERM', () => {
+process.on('SIGTERM', async () => {
   logger.info('SIGTERM received')
   if (server) {
     server.close()
   }
+  await prisma.$disconnect()
+  logger.info('Database connection closed')
 })
